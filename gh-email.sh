d76df10c58654299f239de59bc815154ce5c35a2
r-jb <24355753+r-jb@users.noreply.github.com>
2023-11-09 16:24:25 +0100

    feat: add input file option

--- gh-email.sh
+++ gh-email.sh
@@ -21,6 +21,7 @@ usage() {
 	echo -e "\nUsage: $0 (option) [repo url|local repo|GitHub Org/User]"
 	echo -e '\t-h, --help\t\tPrint this help page'
 	echo -e '\t-o, --output=FILE\tFile to save the output into'
+	echo -e '\t-i, --input=FILE\tFile to read the list of targets from'
 	echo -e '\t-f,--filter=FILE\tFilter out emails containing this filter'
 	echo -e '\t-r, --raw\t\tNo filter no banner'
 	echo -e '\t--exclude-name\t\tExclude authors'\''s name'
@@ -39,12 +40,14 @@ error() {
 }
 
 parse_args() {
-	read -r SCAN_NAME USER_FILTERS OUTPUT_FILE TARGET <<< ''
+	read -r SCAN_NAME USER_FILTERS OUTPUT_FILE INPUT_FILE TARGET <<< ''
 	while (( "$#" )); do
 		case "$1" in
 			--help|-h) usage; exit;;
 			--output=*) OUTPUT_FILE="${1#*=}";;
 			--output |-o ) OUTPUT_FILE=$2; shift;;
+			--input=*) INPUT_FILE="${1#*=}";;
+			--input |-i ) INPUT_FILE=$2; shift;;
 			--filter=*) USER_FILTERS+=" -e ${1#*=}";;
 			--filter |-f ) USER_FILTERS+=" -e ${2}"; shift;;
 			--raw |-r ) USE_FILTERS=false;;
@@ -53,57 +56,103 @@ parse_args() {
 			--exclude-private ) INCLUDE_PRIVATE=false;;
 			--keep |-k ) KEEP_DOWNLOADS=true;;
 			--update |-u ) UPDATE=true;;
-			--no-color ) read -r NO_COLOR BOLD_WHITE YELLOW <<< '';;
+			--no-color ) read -r NO_COLOR GREEN YELLOW BOLD_WHITE <<< '';;
 			*)
 				TARGET="$1"
 		esac
 		shift
 	done
 
-	if [ -n "$TARGET" ] && [ -z "$REPO_LIST" ]; then
+	if [ -n "$TARGET" ]; then
+		if [ -n "$INPUT_FILE" ]; then
+			local repo_list scan_name
+			parse_input_file "$INPUT_FILE"
 
-		# Check if target is a local dir
-		if [ -d "$TARGET" ]; then
-			SCAN_NAME="$(basename "$TARGET")"
-			get_repo_list_local "$TARGET"
+			# If target is not in the input file
+			if ! grep --quiet --max-count 1 "$TARGET" <<< "$REPO_LIST"; then
+				repo_list="$REPO_LIST"
+				scan_name="$SCAN_NAME"
 
-		# Check if target is a remote git repo
-		elif repo_exist_not_empty "$TARGET"; then
-			local _scan_name
-			_scan_name="${TARGET#*.*/}"
-			SCAN_NAME="${_scan_name%.git}"
-			REPO_LIST="$TARGET"
-
-		# Check if URI is a GitHub repo
-		elif [[ "$TARGET" =~ ^[a-zA-Z0-9_.-]+\/[a-zA-Z0-9_.-]+$ ]]; then
-			if repo_exist_not_empty "https://github.com/${TARGET}"; then
-				SCAN_NAME="$TARGET"
-				REPO_LIST="https://github.com/${TARGET}"
-			else
-				error "repository empty"
+				# Add target to the list
+				target_to_repo_list "$TARGET"
+				REPO_LIST+="$repo_list"
+				SCAN_NAME="$scan_name, $SCAN_NAME"
 			fi
-
-		# Otherwise
-		# Check if it is a GitHub Org/User
-		elif gh_owner_exist "$TARGET"; then
-			if gh_owner_has_repo "$TARGET"; then
-				SCAN_NAME="$TARGET"
-				get_gh_owner_repo_list "$TARGET"
-				if [ -z "$REPO_LIST" ] && [ $INCLUDE_FORK = 'false' ]; then
-					error 'owner has no accessible repository matching criterias'
-				fi
-			else
-				error 'owner has no accessible repository'
-			fi
+		else
+			target_to_repo_list "$TARGET"
 		fi
-
-		if [ -z "$REPO_LIST" ]; then
-			error "target not found or empty: $TARGET"
-		fi
-
-	elif [ -z "$TARGET" ]; then
+	elif [ -n "$INPUT_FILE" ]; then
+		parse_input_file "$INPUT_FILE"
+	else
 		error 'no target provided'
 	fi
+}
+
+# Usage: target_to_repo_list TARGET
+# Output: $REPO_LIST
+target_to_repo_list() {
+	local target
+	target="$1"
+
+	# Check if target is a local dir
+	if [ -d "$target" ]; then
+		SCAN_NAME="$(basename "$target")"
+		get_repo_list_local "$target"
+
+	# Check if target is a remote git repo
+	elif repo_exist_not_empty "$target"; then
+		local _scan_name
+		_scan_name="${target#*.*/}"
+		SCAN_NAME="${_scan_name%.git}"
+		REPO_LIST="$target"
+
+	# Check if URI is a GitHub repo
+	elif [[ "$target" =~ ^[a-zA-Z0-9_.-]+\/[a-zA-Z0-9_.-]+$ ]]; then
+		if repo_exist_not_empty "https://github.com/${target}"; then
+			SCAN_NAME="$target"
+			REPO_LIST="https://github.com/${target}"
+		else
+			error "repository empty"
+		fi
+
+	# Otherwise
+	# Check if it is a GitHub Org/User
+	elif gh_owner_exist "$target"; then
+		if gh_owner_has_repo "$target"; then
+			SCAN_NAME="$target"
+			get_gh_owner_repo_list "$target"
+			if [ -z "$REPO_LIST" ] && [ $INCLUDE_FORK = 'false' ]; then
+				error 'owner has no accessible repository matching criterias'
+			fi
+		else
+			error 'owner has no accessible repository'
+		fi
+	fi
+
+	if [ -z "$REPO_LIST" ]; then
+		error "target not found or empty: $target"
+	fi
+}
+
+# Usage: parse_input_file FILE
+# Output: $REPO_LIST
+parse_input_file() {
+	local input_file file_repo_list
+	input_file="$1"
+
+	if [ -f "$input_file" ]; then
+		while read -r line; do
+			if [ -n "$line" ]; then
+				target_to_repo_list "$line"
+				file_repo_list+="$REPO_LIST"
+			fi
+		done <<< "$(cat "$input_file")"
+		SCAN_NAME="$(basename "$input_file")"
+	else
+		error "file not found: $input_file"
+	fi
+
+	REPO_LIST="$file_repo_list"
 }
 
 clean() {
@@ -163,14 +212,14 @@ clone() {
 	if [ -d "$2" ]; then
 		if [ $UPDATE = 'true' ]; then
 			echo -n ' Updating...'
-			if git -C "$2" pull --quiet 2> /dev/null; then
+			if git -C "$2" pull --quiet >/dev/null 2>&1; then
 				ret_error=0
 			else
 				echo -n ' Failed. Downloading...'
 
 				# Attempt to clone the repo in a temp dir
 				if repo_exist_not_empty "$1"; then
-					if git clone --no-checkout --quiet "$1" "_$2" 2> /dev/null; then
+					if git clone --no-checkout --quiet "$1" "_$2" >/dev/null 2>&1; then
 						rm -rf "${2:?}" && \
 						mv -f "_$2" "$2" && \
 						ret_error=0
@@ -188,7 +237,7 @@ clone() {
 	else
 		echo -n ' Downloading...'
 		if repo_exist_not_empty "$1"; then
-			if git clone --no-checkout --quiet "$1" "$2"; then
+			if git clone --no-checkout --quiet "$1" "$2" >/dev/null 2>&1; then
 				ret_error=0
 			else
 				error "repository out of reach: $1"
@@ -245,14 +294,14 @@ output_results() {
 	local author_count
 	author_count="$(wc -l <<< "$1")"
 
-	if [ "$author_count" -le '1' ]; then
-		echo -ne "\n[${GREEN}i${NO_COLOR}] - No email matching criterias found.\n"
+	if [ "$author_count" -le '0' ]; then
+		echo -e "\n[${GREEN}i${NO_COLOR}] - No email matching criterias found."
 	else
 		if [ -n "$OUTPUT_FILE" ]; then
-			echo -ne "\n[${GREEN}i${NO_COLOR}] - Saving to ${BOLD_WHITE}${OUTPUT_FILE}${NO_COLOR}...\n"
-			echo -e "email, names\n$1" > "${OUTPUT_FILE:?}"
+			echo -e "email, names\n$1" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" > "${OUTPUT_FILE:?}"
+			echo -e "\n[${GREEN}i${NO_COLOR}] - Results saved to ${BOLD_WHITE}${OUTPUT_FILE}${NO_COLOR}"
 		else
-			echo -e "\n${1}"
+			echo -e "\n\n${1}"
 		fi
 	fi
 }
@@ -337,11 +386,16 @@ parse_args "$@"
 
 # Handle download dir
 if [ $KEEP_DOWNLOADS = 'true' ]; then
-	TEMP_DIR="$SCAN_NAME"
-	[ ! -d "$TEMP_DIR" ] && mkdir -p "${TEMP_DIR:?}"
+	if [ -n "$INPUT_FILE" ]; then
+		TEMP_DIR="${INPUT_FILE%.*}"
+	else
+		TEMP_DIR="$SCAN_NAME"
+	fi
+	echo -e "\n[${GREEN}i${NO_COLOR}] - Keeping downloaded .git in ${BOLD_WHITE}./${TEMP_DIR}/${NO_COLOR}\n"
 else
 	TEMP_DIR="$(mktemp -d -q)"
 fi
+[ ! -d "$TEMP_DIR" ] && mkdir -p "${TEMP_DIR:?}"
 
 echo -e '---------------------------------------\n'
 echo -e "Starting scan of ${BOLD_WHITE}${SCAN_NAME}${NO_COLOR}"
